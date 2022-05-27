62f9934821a7f47dd7d6cc37957e7ca8e230eef0
FabriJuncal <juncalfabri@gmail.com>
2022-05-26 23:05:30 -0300

    36. Creando controlador para el registro de usuario

---
+++ app/Http/Controllers/RegisterController.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class RegisterController extends Controller
+{
+    //Por convención los controladores deben tener un método llamado "index" cuando este tenga solo 1 método
+    public function index() {
+        return view('auth.register');
+    }
+}
