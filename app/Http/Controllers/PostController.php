9cde84f619bf7a98c2a29deb8e976ac47f8293fc
fjuncal <fabriciojuncal@micam.com.ar>
2022-06-02 22:14:51 -0300

    59. Creando el Muro

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -6,10 +6,18 @@ use Illuminate\Http\Request;
 
 class PostController extends Controller
 {
+    public function __construct()
+    {
+        // Valida que haya un Usuario Autenticado, de esta manera protegemos la ruta en el cual se requiere que el usuario se encuentre logeado
+        $this->middleware('auth');
+    }
+
     public function index()
     {
         // auth()->user() => Esta función sirve para obtener el objeto de autenticación de Laravel.
         //                   Este muestra los datos del usuario que se encuentra autenticado.
-        dd(auth()->user());
+        // dd(auth()->user());
+
+        return view('dashboard');
     }
 }
