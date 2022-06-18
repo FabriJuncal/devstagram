713492ef5b8c0d9d210ef0e4f5db876f8d259de6
fjuncal <fabriciojuncal@micam.com.ar>
2022-06-18 13:34:45 -0300

    82. Factories para realizar Testing a la base de datos

--- database/factories/PostFactory.php
+++ database/factories/PostFactory.php
@@ -17,7 +17,10 @@ class PostFactory extends Factory
     public function definition()
     {
         return [
-            //
+            'titulo' => $this->faker->sentence(5), // Genera una frase de 5 palabras
+            'descripcion' => $this->faker->sentence(20), // Genera una frase de 20 palabras
+            'imagen' => $this->faker->uuid() . 'jpg', // Genera un UUID y le agrega .jpg
+            'user_id' => $this->faker->randomElement([1, 2, 3]) // Genera un numero aleatorio entre 1 y 3
         ];
     }
 }
