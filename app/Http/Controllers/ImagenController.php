b3a1cf9c3a974d1ae743de514fe14900d96cd7a2
fjuncal <fabriciojuncal@micam.com.ar>
2022-06-17 22:10:42 -0300

    79. Almacenando las imágenes en el Servidor

--- app/Http/Controllers/ImagenController.php
+++ app/Http/Controllers/ImagenController.php
@@ -3,6 +3,8 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Str;
+use Intervention\Image\Facades\Image;
 
 class ImagenController extends Controller
 {
@@ -10,7 +12,21 @@ class ImagenController extends Controller
     // (Request $request) => Instanciamos la clase Request para obtener los datos de la petición
     public function store(Request $request)
     {
+        // Obtenemos los datos del archivo que se subió
         $imagen = $request->file('file');
+
+        // Creamos un nombre aleatorio para el archivo
+        $nombreImagen = Str::uuid() . "." . $imagen->extension();
+
+        // Pasamos como parametro la imagen que se quiere subír a la función de Intervention.io
+        // De esta forma vamos a poder manipular la imagen antes de subirla al servidor
+        $imagenServidor = Image::make($imagen);
+        $imagenServidor->fit(1000, 1000, null, 'center'); // Redimensionamos la imagen a 1000x1000px y definimos que corte en el centro
+
+        // Definimos la ruta donde vamos a almacenar la imagen
+        $imagenPath = public_path('uploads/' . $nombreImagen);
+        // Guardamos la imagen en la ruta definida
+        $imagenServidor->save($imagenPath);
 
         // response()->json() => Es un Helper de Laravel que se utiliza para devolver una respuesta en formato JSON
         return response()->json(['imagen' => "Probando Respuesta"]);
