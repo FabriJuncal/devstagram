9ca5e22ca891c718ee47d04b922e19bf07f55af1
fjuncal <fabriciojuncal@micam.com.ar>
2022-07-21 22:04:09 -0300

    131. Creando el Modelo y Migración

---
+++ database/migrations/2022_07_22_005316_create_likes_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('likes', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('user_id')->constrained()->onDelete('cascade'); // ->onDelete('cascade'): Elimina los registro de la tabla "likes" cuando se elimina el usuario relacionado
+            $table->foreignId('post_id')->constrained()->onDelete('cascade'); // ->onDelete('cascade'): Elimina los registro de la tabla "likes" cuando se elimina el post relacionado
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('likes');
+    }
+};
