3b9b719bc87a3f1e4a30560f9b30b5a9492b11ac
fjuncal <fabriciojuncal@micam.com.ar>
2022-07-21 22:21:41 -0300

    132. Creando el botón de Like

--- resources/views/posts/show.blade.php
+++ resources/views/posts/show.blade.php
@@ -12,8 +12,20 @@
     <div class="md:w-6/12 max-w-lg">
         <img src="{{ asset('uploads') . '/' . $post->imagen }}" alt="Imagen del post {{ $post->titulo }}">
 
-        <div class="p-3">
-            0 Likes
+        <div class="p-3 flex items-center gap-4">
+            <form action="">
+                <div class="my-4">
+                    <button type="submit">
+                        <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">
+                            <path stroke-linecap="round" stroke-linejoin="round" d="M4.318 6.318a4.5 4.5 0 000 6.364L12 20.364l7.682-7.682a4.5 4.5 0 00-6.364-6.364L12 7.636l-1.318-1.318a4.5 4.5 0 00-6.364 0z" />
+                          </svg>
+                    </button>
+
+                </div>
+            </form>
+
+            <p>0 Likes</p>
+
         </div>
 
         <div>
