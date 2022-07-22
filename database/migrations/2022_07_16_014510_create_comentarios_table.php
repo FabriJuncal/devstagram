86e63079674ba49964f75840682918f40f50f5da
fjuncal <fabriciojuncal@micam.com.ar>
2022-07-21 21:49:42 -0300

    130. ¿Para que sirven los constrained en las migraciones?

--- database/migrations/2022_07_16_014510_create_comentarios_table.php
+++ database/migrations/2022_07_16_014510_create_comentarios_table.php
@@ -16,8 +16,8 @@ return new class extends Migration
         // Crear la tabla comentarios (de los post) en la base de datos con los siguientes campos:
         Schema::create('comentarios', function (Blueprint $table) {
             $table->id(); // id de la tabla
-            $table->foreignId('user_id')->constrained(); // id de la tabla users
-            $table->foreignId('post_id')->constrained(); // id de la tabla posts
+            $table->foreignId('user_id')->constrained()->onDelete('cascade'); // ->onDelete('cascade'): Elimina los registro de la tabla "comentarios" cuando se elimina el usuario relacionado
+            $table->foreignId('post_id')->constrained()->onDelete('cascade');; // ->onDelete('cascade'): Elimina los registro de la tabla "comentarios" cuando se elimina el post relacionado
             $table->string('comentario'); // comentario de los post
             $table->timestamps(); // crea dos campos, created_at y updated_at
         });
