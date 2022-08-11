7bcd5c9a4de81ab5272f8bbf58c0d1771d9c5e8f
fjuncal <fabriciojuncal@micam.com.ar>
2022-08-10 22:38:41 -0300

    157. Revisando que todo funcione a la perfección

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -8,13 +8,22 @@ use Illuminate\Http\Request;
 class HomeController extends Controller
 {
 
+    // El método __construct se ejecuta automáticamente cuando se crea una instancia de la clase
+    public function __construct()
+    {
+        // El método middleware se encarga de verificar si el usuario está autenticado o no
+        // De esta manera protegemos las rutas que requieren autenticación
+        // Si el usuario no está autenticado se redirecciona a la ruta login
+        $this->middleware('auth');
+    }
+
     // __invoke => Método que se ejecuta cuando se invoca la clase
     function __invoke()
     {
         // Obtener a quienes seguimos
         $ids = auth()->user()->followings->pluck('id')->toArray();
         // Filtramos los Post por los Usuarios que seguimos
-        $posts = Post::whereIn('user_id', $ids)->paginate(20);
+        $posts = Post::whereIn('user_id', $ids)->latest()->paginate(20);
 
         return view('home', compact('posts'));
     }
