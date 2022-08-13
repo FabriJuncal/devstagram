8b66ec1b344375dc92d2e8922e82536601d13a25
fjuncal <fabriciojuncal@micam.com.ar>
2022-08-13 20:27:48 -0300

    166. Interfaces Reactivas con Livewire

--- app/Http/Livewire/LikePost.php
+++ app/Http/Livewire/LikePost.php
@@ -11,6 +11,17 @@ class LikePost extends Component
     // <livewire:like-post :post="$post"/>
     public $post;
 
+    // Atributo que se usa para saber si el usuario ya ha dado like al post
+    public $isLiked;
+
+    // mount() es un método que se ejecuta automáticamente cuando el componente se carga,
+    // vendría a ser como el constructor del componente, donde este se ejecuta cuando se instancía o se carga el componente.
+    public function mount()
+    {
+        $this->isLiked = $this->post->checkLike(auth()->user());
+    }
+
+
     // Método que se ejecuta cuando se hace click en el botón de like
     // Este método se encargará de realizar la acción de like o dislike
     public function like()
@@ -19,11 +30,13 @@ class LikePost extends Component
         if( $this->post->checkLike(auth()->user()) ) {
             // Si ya ha dado like al post, entonces se elimina el like
             $this->post->likes()->where('post_id', $this->post->id)->delete();
+            $this->isLiked = false;
         } else {
             // Si no ha dado like al post, entonces se crea un nuevo like
             $this->post->likes()->create([
                 'user_id' => auth()->user()->id
             ]);
+            $this->isLiked = true;
         }
 
 
