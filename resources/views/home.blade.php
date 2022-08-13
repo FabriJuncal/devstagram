a4f300a7478518aa24b5c5676709a7ba48fd7fa4
fjuncal <fabriciojuncal@micam.com.ar>
2022-08-13 17:44:08 -0300

    159. Que son los $slots en los Componentes

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -9,6 +9,26 @@
 
     {{-- Componente de Laravel: --}}
     {{-- Todos los componentes de laravel comienzan con "x-" --}}
+    {{-- Los componentes que no vayan a recibír parametros deben ser de una sola etiqueta y deben cerrarse con "/>" --}}
     <x-listar-post/>
+
+    {{-- A los componentes de Laravel se le pueden pasar parametros de las siguientes maneras: --}}
+    {{-- Forma 1 --}}
+    {{-- <h1> => No definimos nombre del Slot (Parametro que se envía al componente) por lo general este parametro hace referencia al nombre $slot dentro del componente--}}
+    {{-- <x-listar-post>
+        <h1>Mostrando post desde Slot</h1>
+    </x-listar-post> --}}
+
+    {{-- Forma 2 --}}
+    {{-- <x-slot:titulo> => Definimos el Nombre del Slot (Parametro que se envía al componente) --}}
+    {{-- <h1> => No definimos nombre del Slot (Parametro que se envía al componente) por lo general este parametro hace referencia al nombre $slot dentro del componente--}}
+    {{-- <x-listar-post>
+        <x-slot:titulo>
+            <header>Esto es un header</header>
+        </x-slot:titulo>
+        <h1>Mostrando post desde Slot</h1>
+    </x-listar-post> --}}
+
+
 
 @endsection
