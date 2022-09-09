a68e46a42b4314923601f5389394a85403949d61
fjuncal <fabriciojuncal@micam.com.ar>
2022-09-09 20:30:58 -0300

    DEPLOYMENT | Fix en los Post

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -4,7 +4,7 @@ namespace App\Models;
 
 use App\Models\User;
 use App\Models\Comentario;
-use App\Models\like;
+use App\Models\Like;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use PhpParser\Node\Stmt\Return_;
